b2f6ff2cac9af2a8de893c11139a985bd90b7c94
S00188023 <S00188023@users.noreply.github.com>
2019-12-11 11:32:04 +0000

    OOPXmasExam2019

--- OOPXmasExam2019/OOPXmasExam2019/MainWindow.xaml.cs
+++ OOPXmasExam2019/OOPXmasExam2019/MainWindow.xaml.cs
@@ -20,10 +20,53 @@ namespace OOPXmasExam2019
     /// </summary>
     public partial class MainWindow : Window
     {
-        public enum Position { Goalkeeper, Defender, Midfielder, Forward}
+        List<Player> allPlayers = new List<Player>();
+        List<Player> selectedPlayers = new List<Player>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] firstNames = {
+                "Adam", "Amelia", "Ava", "Chloe", "Conor", "Daniel", "Emily",
+                "Emma", "Grace", "Hannah", "Harry", "Jack", "James",
+                "Lucy", "Luke", "Mia", "Michael", "Noah", "Sean", "Sophie"};
+
+
+            string[] lastNames = {
+                "Brennan", "Byrne", "Daly", "Doyle", "Dunne", "Fitzgerald", "Kavanagh",
+                "Kelly", "Lynch", "McCarthy", "McDonagh", "Murphy", "Nolan", "O'Brien",
+                "O'Connor", "O'Neill", "O'Reilly", "O'Sullivan", "Ryan", "Walsh"
+            };
+
+            Player aPlayer = new Player("Adam ", "Brennan ", Position.Defender, new DateTime(1995, 05, 11), 24);
+            Player bPlayer = new Player("Conor ", "Doyle ", Position.Forward, new DateTime(1995, 05, 11), 24);
+            Player cPlayer = new Player("Emma ", "Kelly ", Position.Goalkeeper, new DateTime(1995, 05, 11), 24);
+            Player dPlayer = new Player("Jack ", "McDonagh ", Position.Midfielder, new DateTime(1995, 05, 11), 24);
+            Player ePlayer = new Player("Sophie ", "Walsh ", Position.Goalkeeper, new DateTime(1995, 05, 11), 24);
+
+            allPlayers.Add(aPlayer);
+            allPlayers.Add(bPlayer);
+            allPlayers.Add(cPlayer);
+            allPlayers.Add(dPlayer);
+            allPlayers.Add(ePlayer);
+
+            lbxAllPlayers.ItemsSource = allPlayers;
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Player selectedPlayer = lbxSelectedPlayers.SelectedItem as Player;
+            /*
+            if(selectedPlayer != null)
+            {
+                allPlayers.Remove(selectedPlayer);
+                selectedPlayers.Add(selectedPlayer);
+
+                */
         }
     }
 }
--- OOPXmasExam2019/OOPXmasExam2019/Player.cs
+++ OOPXmasExam2019/OOPXmasExam2019/Player.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace OOPXmasExam2019
 {
+    public enum Position { Goalkeeper, Defender, Midfielder, Forward }
     public class Player
     {
         public string FirstName { get; set; }
@@ -13,5 +14,23 @@ namespace OOPXmasExam2019
         public Position PreferedPosition { get; set; }
         public DateTime DateOfBirth { get; set; }
         public int Age { get; set; }
+
+
+        public Player(string firstName, string lastName, Position position, DateTime dateOfBirth, int age)
+        {
+            FirstName = firstName;
+            Surname = lastName;
+            PreferedPosition = position;
+            DateOfBirth = dateOfBirth;
+            Age = age;
+        }
+        
+
+        public override string ToString()
+        {
+            return string.Format("{0 } {1 } {2 } {3}", FirstName, Surname, Age, PreferedPosition);
+        }
+
+
     }
 }
